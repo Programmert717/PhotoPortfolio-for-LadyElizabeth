a538cafbd9a311994cbf4ea1ada22ea25bccdb73
Programmert717 <milan.kalashnikov2017@mail.ru>
2025-05-28 12:03:28 +0300

    root fix

--- queenfolio/urls.py
+++ queenfolio/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='home'), name='logout'),
-    path('super_lizalalala/', include('gallery.urls')),
+    path('', include('gallery.urls')),
 ]
 
 if settings.DEBUG:
